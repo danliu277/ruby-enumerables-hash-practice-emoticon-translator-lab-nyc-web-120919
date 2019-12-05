bbbf02c1b8aa3bac52051770185a388064597d48
Daniel Liu <danliu277@gmail.com>
2019-12-05 19:44:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(library)
     # pp "meaning", meaning, "array", array
     # pp "english", english
     # pp "japanese", japanese
-    result["get_emoticon"][english] = japanese
+    result["get_emoticon"][english] = meaning
     result["get_meaning"][japanese] = meaning
   end
   pp result
