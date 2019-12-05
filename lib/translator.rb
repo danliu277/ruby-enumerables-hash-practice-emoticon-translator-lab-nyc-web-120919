508ef6a28b08160e86b9d83b5d45b52a6a9b4b48
Daniel Liu <danliu277@gmail.com>
2019-12-05 19:42:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(library)
   }
   data.each do |meaning, array|
     english, japanese = array
-    pp meaning, array
+    pp "meaning", meaning, "array", array
     pp "english", english
     pp "japanese", japanese
     result["get_emoticon"][english] = japanese
