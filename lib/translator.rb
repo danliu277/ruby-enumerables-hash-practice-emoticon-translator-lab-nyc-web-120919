1dbca0f1d8372764809f2432010b965e031053c5
Daniel Liu <danliu277@gmail.com>
2019-12-05 19:37:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,19 +2,13 @@
 require "yaml"
 
 def load_library(library)
-  # code goes here
-  data = YAML.load_file(library)
-  result = {
-    "get_meaning" => {},
-    "get_emoticion" => {}
-  }
-  data.each do |meaning, array|
-    # puts meaning, array
+  result = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(library).each do |meaning, array|
     english, japanese = array
-    result["get_emoticion"][english] = japanese
+    result["get_emoticon"][english] = japanese
     result["get_meaning"][japanese] = meaning
   end
-  return result
+  result
 end
 
 def get_japanese_emoticon
