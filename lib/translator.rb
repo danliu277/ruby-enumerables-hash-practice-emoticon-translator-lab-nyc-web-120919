872b19ab636ffe66e29bf95ade127be5ce70b2f5
Daniel Liu <danliu277@gmail.com>
2019-12-05 19:33:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,8 @@ def load_library(library)
   data.each do |meaning, array|
     # puts meaning, array
     english, japanese = array
-    result["get_meaning"][english] = array
-    result["get_emoticion"][japanese] = array
+    result["get_meaning"][english] = english
+    result["get_emoticion"][japanese] = japanese
   end
   return result
 end
